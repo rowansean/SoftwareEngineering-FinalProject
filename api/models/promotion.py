43ccc89fe1a59c49915506788908653e085b9c2a
alfredorodriguez <alfredorodriguez93@gmail.com>
2024-04-18 11:53:21 -0400

    added promotion files

---
+++ api/models/promotion.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
+
+
+class Promotion(Base):
+    __tablename__ = "promotions"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    promotion_code = Column(String, unique=True)
+    expiration_date = Column(DATETIME)
+
+    promotion = relationship("Promotion", back_populates="payment_information")
