3755723a36c7eb380b9572d83d6cd3cbf5438ffd
alfredorodriguez <alfredorodriguez93@gmail.com>
2024-05-02 21:20:33 -0400

    newest changes

--- api/models/orders.py
+++ api/models/orders.py
@@ -11,8 +11,8 @@ class Order(Base):
     customer_id = Column(Integer, ForeignKey("customers.id"))
     order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
     description = Column(String(300))
-    customer_name = Column(String, index=True)
+    customer_name = Column(String(100), index=True)
 
-    order_details = relationship("OrderDetail", back_populates="order", uselist=False)
+    order_details = relationship("OrderDetail", back_populates="order")
     customer = relationship("Customer", back_populates="orders")
     reviews = relationship("Review", back_populates="order")
